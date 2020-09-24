f3dfd66b790e64703833efc5b33d6f16b9e9e794
mastermaarten <mastermaarten99@gmail.com>
2020-09-24 14:39:22 +0200

    PlayerOverview Layout Improved + Unit test setup
    Header zou nog verbeterd kunnen worden.
    Mist nog Unit Tests en code improvement.

---
+++ DARTS_UnitTests/Windows/PlayersOverviewWindow_UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DARTS_UnitTests.Windows
+{
+    [TestClass]
+    public class PlayersOverviewWindow_UnitTest
+    {
+        [TestMethod]
+        public void CreatePlayerOverviewFilterTests()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+           
+        }
+    }
+}
