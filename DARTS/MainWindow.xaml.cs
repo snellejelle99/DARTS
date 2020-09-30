2595499b09fa5eceb5ef9b2c52a3cfc2ab17c3f8
Jan <janvandertuin@live.nl>
2020-09-30 13:29:32 +0200

    ..

--- DARTS/MainWindow.xaml.cs
+++ DARTS/MainWindow.xaml.cs
@@ -1,14 +1,25 @@
-﻿< Window x: Class = "DARTS.MainWindow"
-        xmlns = "http://schemas.microsoft.com/winfx/2006/xaml/presentation"
-        xmlns: x = "http://schemas.microsoft.com/winfx/2006/xaml"
-        xmlns: d = "http://schemas.microsoft.com/expression/blend/2008"
-        xmlns: mc = "http://schemas.openxmlformats.org/markup-compatibility/2006"
-        xmlns: local = "clr-namespace:DARTS"
-        mc: Ignorable = "d"
-        Title = "MainWindow" Height = "450" Width = "800" >
-    
-        < Grid >
-    
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
-        </ Grid >
-    </ Window >
+namespace DARTS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
