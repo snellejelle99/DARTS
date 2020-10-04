e4ad3ba9c081a0338fa6b4af03001c4760107fde
demistedehouder <32260344+demistedehouder@users.noreply.github.com>
2020-10-04 14:42:36 +0200

    added basic main menu, started building it so it fits MVVM pattern, needs refactoring of some sort

---
+++ DARTS/ViewModel/MainMenuViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using DARTS.Data;
+using DARTS.Data.DataObjects;
+using DARTS.View;
+using DARTS.ViewModel.Command;
+
+namespace DARTS.ViewModel
+{
+    class MainMenuViewModel
+    {
+        private MainMenuView _view;
+        public ICommand StartMatchButtonClickCommand { get; }
+        public ICommand PlayerOverviewButtonClickCommand { get; }
+        public ICommand MatchOverviewButtonClickCommand { get; }
+
+        public MainMenuViewModel(MainMenuView view)
+        {
+            StartMatchButtonClickCommand = new RelayCommand(execute => StartMatchButton_Click(), canExecute => CanExecuteStartMatchButtonClick());
+            PlayerOverviewButtonClickCommand = new RelayCommand(execute => PlayerOverviewButton_Click(), canExecute => CanExecutePlayerOverviewButtonClick());
+            MatchOverviewButtonClickCommand = new RelayCommand(execute => MatchOverviewButton_Click(), canExecute => CanExecuteMatchOverviewButtonClick());
+
+            view.DataContext = this;
+            _view = view;
+            _view.Show();
+        }
+
+        private void StartMatchButton_Click()
+        {
+
+        }
+
+        private bool CanExecuteStartMatchButtonClick()
+        {
+            return true;
+        }
+
+        private void PlayerOverviewButton_Click()
+        {
+
+        }
+
+        private bool CanExecutePlayerOverviewButtonClick()
+        {
+            return true;
+        }
+
+        private void MatchOverviewButton_Click()
+        {
+
+        }
+
+        private bool CanExecuteMatchOverviewButtonClick()
+        {
+            return true; 
+        }
+    }
+}
