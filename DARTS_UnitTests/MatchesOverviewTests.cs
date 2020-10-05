09020a13f24c5f397d9a6ff0335209af6f5629ff
Christiaan Bakker <32450195+skiephole@users.noreply.github.com>
2020-10-05 11:36:09 +0200

    Added test class

---
+++ DARTS_UnitTests/MatchesOverviewTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DARTS_UnitTests
+{
+    [TestClass]
+    class MatchesOverviewTests
+    {
+    }
+}
