cde62a6e0d8e024fb059462ace819d2e99c45944
Jan <janvandertuin@live.nl>
2020-10-06 16:19:48 +0200

    Implemented MVVM pattern for score input and score overview
    The score input and score overview now follows the MVVM pattern. + code cleanup

--- DARTS/App.xaml.cs
+++ DARTS/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DARTS.View;
+using DARTS.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,16 @@ namespace DARTS
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            ScoreInputView window = new ScoreInputView
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterScreen
+            };
+            ScoreInputViewModel viewModel = new ScoreInputViewModel();
+
+            window.DataContext = viewModel;
+            window.Show();       
+        }
     }
 }
--- DARTS/ViewModel/ScoreOverviewViewModel.cs
+++ DARTS/ViewModel/ScoreOverviewViewModel.cs
@@ -1,29 +1,17 @@
-﻿using System;
+﻿using DARTS.ViewModel.Command;
+using System;
 using System.Collections.Generic;
+using System.IO.Packaging;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace DARTS.User_Controls
+namespace DARTS.ViewModel
 {
-    /// <summary>
-    /// Interaction logic for ScoreOverzicht.xaml
-    /// </summary>
-    public partial class ScoreOverzicht : UserControl
+    class ScoreOverviewViewModel
     {
-        public ScoreOverzicht()
-        {
-            InitializeComponent();
-            this.DataContext = this;
-        }
-        
         public string Player1 { get; set; }
         public string ScorePlayer1 { get; set; }
         public string ScoreNeedPlayer1 { get; set; }
@@ -35,6 +23,23 @@ namespace DARTS.User_Controls
         public string ScoreNeedPlayer2 { get; set; }
         public string SetsPlayer2 { get; set; }
         public string LegsPlayer2 { get; set; }
-       
+
+        public ScoreOverviewViewModel()
+        {
+            Player1 = "Jan";
+            Player2 = "Jan2";
+
+            ScorePlayer1 = "100";
+            ScorePlayer2 = "100";
+
+            ScoreNeedPlayer1 = "401";
+            ScoreNeedPlayer2 = "401";
+
+            SetsPlayer1 = "1";
+            SetsPlayer2 = "2";
+
+            LegsPlayer1 = "2";
+            LegsPlayer2 = "1";
+        }
     }
 }
