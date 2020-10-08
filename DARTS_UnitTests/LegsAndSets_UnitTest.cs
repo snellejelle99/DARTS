da4a38f2699a5b437415fb91e60e6e29019af336
demistedehouder <32260344+demistedehouder@users.noreply.github.com>
2020-10-08 16:08:50 +0200

    resolved failing tests

--- DARTS_UnitTests/LegsAndSets_UnitTest.cs
+++ DARTS_UnitTests/LegsAndSets_UnitTest.cs
@@ -13,14 +13,14 @@ namespace DARTS_UnitTests
         public void Should_Only_Accept_Odd_Numbers_When_Int_Is_Inputed()
         {
             Assert.IsTrue(legValidator.Validate(1, null).IsValid);
-            Assert.IsTrue(legValidator.Validate(4, null).IsValid);
+            Assert.IsFalse(legValidator.Validate(4, null).IsValid);
             Assert.IsTrue(legValidator.Validate(23, null).IsValid);
-            Assert.IsTrue(legValidator.Validate(8, null).IsValid);
+            Assert.IsFalse(legValidator.Validate(8, null).IsValid);
 
             Assert.IsTrue(setValidator.Validate(9, null).IsValid);
             Assert.IsTrue(setValidator.Validate(67, null).IsValid);
-            Assert.IsTrue(setValidator.Validate(44, null).IsValid);
-            Assert.IsTrue(setValidator.Validate(12, null).IsValid);
+            Assert.IsFalse(setValidator.Validate(44, null).IsValid);
+            Assert.IsFalse(setValidator.Validate(12, null).IsValid);
         }
     }
 }
