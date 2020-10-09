a1e25ce3d15a12450e906e8f91296ceada86a570
demistedehouder <32260344+demistedehouder@users.noreply.github.com>
2020-10-09 12:48:32 +0200

    unit test for checking if textboxes accept only odd numbers

--- DARTS_UnitTests/LegsAndSets_UnitTest.cs
+++ DARTS_UnitTests/LegsAndSets_UnitTest.cs
@@ -6,17 +6,21 @@ namespace DARTS_UnitTests
     [TestClass]
     class LegsAndSets_UnitTest
     {
-        private LegsValidationRule legValidator = new LegsValidationRule();
-        private SetsValidationRule setValidator = new SetsValidationRule();
+        private readonly LegsValidationRule legValidator = new LegsValidationRule();
+        private readonly SetsValidationRule setValidator = new SetsValidationRule();
 
         [TestMethod]
-        public void Should_Only_Accept_Odd_Numbers_When_Int_Is_Inputed()
+        public void Legs_Should_Only_Accept_Odd_Numbers_When_Value_Is_Entered()
         {
             Assert.IsTrue(legValidator.Validate(1, null).IsValid);
             Assert.IsFalse(legValidator.Validate(4, null).IsValid);
             Assert.IsTrue(legValidator.Validate(23, null).IsValid);
             Assert.IsFalse(legValidator.Validate(8, null).IsValid);
+        }
 
+        [TestMethod]
+        public void Sets_Should_Only_Accept_Odd_Numbers_When_Value_Is_Entered()
+        {
             Assert.IsTrue(setValidator.Validate(9, null).IsValid);
             Assert.IsTrue(setValidator.Validate(67, null).IsValid);
             Assert.IsFalse(setValidator.Validate(44, null).IsValid);
