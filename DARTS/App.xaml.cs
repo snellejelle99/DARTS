f7e46766056646c883a260d03f9329bbef638316
SwenvdWijngaard <32259799+SwenvdWijngaard@users.noreply.github.com>
2020-10-12 21:23:54 +0200

    ScoreInput Refactor
    - Added match overview with databinded variables.
    - Added Previous turn button.
    - Added Stop Match button.
    - Implemented textbox and comboBox validation.

--- DARTS/App.xaml.cs
+++ DARTS/App.xaml.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DARTS.Data;
 
 namespace DARTS
 {
@@ -17,11 +18,18 @@ namespace DARTS
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            MainMenuView window = new MainMenuView
+            //MainMenuView window = new MainMenuView
+            //{
+            //    WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen
+            //};
+            //MainMenuViewModel viewModel = new MainMenuViewModel();
+
+            ScoreInputView window = new ScoreInputView
             {
                 WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen
             };
-            MainMenuViewModel viewModel = new MainMenuViewModel();
+            ScoreInputViewModel viewModel = new ScoreInputViewModel(DummyData.GetDummyMatch());
+
             window.DataContext = viewModel;
 
             window.Show();
