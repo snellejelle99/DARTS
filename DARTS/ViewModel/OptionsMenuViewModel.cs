2abedc9f3710fba698aa7404738d0ff260dd1f82
demistedehouder <32260344+demistedehouder@users.noreply.github.com>
2020-10-13 11:13:28 +0200

    added command functions to buttons on optionsmenu, made go back to main menu button functional

--- DARTS/ViewModel/OptionsMenuViewModel.cs
+++ DARTS/ViewModel/OptionsMenuViewModel.cs
@@ -15,5 +15,41 @@ namespace DARTS.ViewModel
 {
     class OptionsMenuViewModel
     {
+        public ICommand ResetDatabaseButtonClickCommand { get; }
+        public ICommand GoBackButtonClickCommand { get; }
+
+        public OptionsMenuViewModel()
+        {
+            ResetDatabaseButtonClickCommand = new RelayCommand(execute => ResetDatabaseButton_Click(execute), canExecute => CanExecuteResetDatabaseButtonClick());
+            GoBackButtonClickCommand = new RelayCommand(execute => GoBackButton_Click(execute), canExecute => CanExecuteGoBackButtonClick());
+        }
+
+        private void ResetDatabaseButton_Click(object parameter)
+        {
+
+        }
+
+        public bool CanExecuteResetDatabaseButtonClick()
+        {
+            return true;
+        }
+
+        private void GoBackButton_Click(object parameter)
+        {
+            MainMenuView mainMenuWindow = new MainMenuView
+            {
+                WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen
+            };
+            MainMenuViewModel mainMenuWindowModel = new MainMenuViewModel();
+            mainMenuWindow.DataContext = mainMenuWindowModel;
+            mainMenuWindow.Show();
+
+            (parameter as Window).Close();
+        }
+
+        public bool CanExecuteGoBackButtonClick()
+        {
+            return true;
+        }
     }
 }
